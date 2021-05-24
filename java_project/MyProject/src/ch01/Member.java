62447f14de50448d29f72a7abad8ff68d9195684
jiwon <xxxlikeag1_@naver.com>
2021-05-24 17:55:29 +0900

    Chapter01

--- java_project/MyProject/src/ch01/Member.java
+++ java_project/MyProject/src/ch01/Member.java
@@ -1,10 +1,22 @@
 package ch01;
 
 public class Member {
-
+	
 	public static void main(String[] args) {
 
 		System.out.println("안녕하세요. 황지원입니다.");
+		System.out.println();
+		
+		String name = "짱구";
+		int age = 10;
+		double height = 170;
+		boolean hasBook = true;
+		
+		System.out.println("이름: " + name);
+		System.out.println("나이: " + age);
+		System.out.println("키: " + height);
+		System.out.println("책 보유여부: " + hasBook);
+	
 	}
 
 }
