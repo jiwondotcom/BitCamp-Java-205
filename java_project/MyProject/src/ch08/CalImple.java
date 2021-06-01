05a10b2c3181ad40ac28ebbf1efc5224fa62e6c1
jiwon <xxxlikeag1_@naver.com>
2021-06-01 16:51:29 +0900

    0601_re

--- java_project/MyProject/src/ch08/CalImple.java
+++ java_project/MyProject/src/ch08/CalImple.java
@@ -1,8 +1,10 @@
 package ch08;
 
 	//Calculator 인터페이스를 상속하는 인스턴스를 생성할 수 있는 클래스를 정의해봅시다.
-	public class CalImple extends CalAbstract implements Calculator {
-	
+	public class CalImple implements Calculator {
+	// 메소드를 완성했기 때문에 인스턴스 생성이 가능하다.
+		
+		@Override
 		public long add(long n1, long n2) {
 			return n1 + n2;
 		}
--- java_project/MyProject/src/ch08/Calculator.java
+++ java_project/MyProject/src/ch08/Calculator.java
@@ -13,7 +13,7 @@ package ch08;
 
 	public interface Calculator { 
 	
-		long add(long n1, long n2); 
+		long add(long n1, long n2); // public abstract
 	    long substract(long n1, long n2 ); 
 	    long multiply(long n1, long n2 ); 
 	    double divide(double n1, double n2 ); 
