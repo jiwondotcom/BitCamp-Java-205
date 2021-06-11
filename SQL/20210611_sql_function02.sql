c4db06ebd06236fd26049083f747b0100290e8c8
jiwon <xxxlikeag1_@naver.com>
2021-06-11 12:23:34 +0900

    SQL

--- SQL/20210611_sql_function02.sql
+++ SQL/20210611_sql_function02.sql
@@ -10,16 +10,14 @@
 -- 3. 급여 평균
 -- 4. 최대 급여
 -- 5. 최소 급여
-select count (*) as "사원의 수",
-         sum(sal) as "급여의 총합",
-         round (avg(sal), 2) as "급여 평균", -- 소수점 둘째자리까지, 셋째 자리에서 반올림 (round 함수 사용)
-         max(sal) as "최대 급여",
-         min(sal) as "최소 급여"
-from emp
-;
-
-select sum(comm) as "커머션의 총합",
-         avg(comm) as "커머션의 평균"
+select count(*) as "사원의 수", 
+       sum(sal) as "급여의 총합",
+       round(avg(sal), 2) as "급여 평균",
+       max(sal) as "최대 급여",
+       min(sal) as "최소 급여",
+       sum(comm) as "커미션의 총합", -- 그룹함수는 nul 값을 제외하고 연산
+       avg(comm) as "커미션의 평균",
+       count(comm) as "커미션 받는 사원의 수"
 from emp;
 
 
@@ -39,11 +37,11 @@ order by publisher
 
 -- 특정 컬럼을 이용해서 그룹핑하기 -> group by
 -- 특정 조건을 기준으로 그룹핑하여 해당 특정 raw 데이터들을 다룬다. 
--- select
+-- select 그룹핑할 기준의 컬럼, 그룹함수
 -- from 테이블
 -- where
 -- group by 그룹핑할 기준의 컬럼
--- 위치 중요!
+-- *** 위치와 순서 중요!
 
 
 
