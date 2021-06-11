7e5e81126fc5ebdb1e6a33da3565d304f635cc4d
jiwon <xxxlikeag1_@naver.com>
2021-06-11 12:48:07 +0900

    SQL_그룹함수

--- SQL/20210611_sql_function02.sql
+++ SQL/20210611_sql_function02.sql
@@ -11,13 +11,13 @@
 -- 4. 최대 급여
 -- 5. 최소 급여
 select count(*) as "사원의 수", 
-       sum(sal) as "급여의 총합",
-       round(avg(sal), 2) as "급여 평균",
-       max(sal) as "최대 급여",
-       min(sal) as "최소 급여",
-       sum(comm) as "커미션의 총합", -- 그룹함수는 nul 값을 제외하고 연산
-       avg(comm) as "커미션의 평균",
-       count(comm) as "커미션 받는 사원의 수"
+         sum(sal) as "급여의 총합",
+         round(avg(sal), 2) as "급여 평균",
+         max(sal) as "최대 급여",
+         min(sal) as "최소 급여",
+         sum(comm) as "커미션의 총합", -- 그룹함수는 null 값을 제외하고 연산
+         round (avg(comm), 0) as "커미션의 평균",
+         count(comm) as "커미션 받는 사원의 수"
 from emp;
 
 
@@ -42,10 +42,38 @@ order by publisher
 -- where
 -- group by 그룹핑할 기준의 컬럼
 -- *** 위치와 순서 중요!
+select avg(sal) from emp where deptno = 10;
+select avg(sal) from emp where deptno = 20;
+select avg(sal) from emp where deptno = 30;
+
+-- 부서번호 그룹별
+select deptno as "부서 번호", count(*) as "부서별 인원", sum(sal) as "부서별 급여 총합", 
+          trunc (avg(sal)) as "부서별 급여 평균", max(sal) as "부서별 최대 급여액", min(sal) as "부서별 최소 급여액"
+from emp
+group by deptno -- 부서번호로 그룹핑한다.
+order  by deptno -- 부서번호로 정렬
+;
+
+select *
+from emp
+order by deptno
+;
 
 
+-- 직급별
+-- group by 대상 : job
 
+--select count(distinct job) from emp;
+select distinct job -- 중복을 제외한 직급 : 5개
+from emp
+;
+-- 그룹핑이 5개가 된다. 
 
+select job as "직급", count(*) as "직급별 인원"
+from emp
+group by job
+order by job
+;
 
 
 
