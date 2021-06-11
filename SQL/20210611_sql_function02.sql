72436c43e2f9e0ec13ecee156303549304292ec8
jiwon <xxxlikeag1_@naver.com>
2021-06-11 12:21:00 +0900

    SQL

--- SQL/20210611_sql_function02.sql
+++ SQL/20210611_sql_function02.sql
@@ -1,4 +1,4 @@
--- 2021.06.11
+-- 2021.06.11 (FRI)
 
 -- 그룹함수
 -- 여러행을 묶어 처리하는 함수
@@ -19,4 +19,35 @@ from emp
 ;
 
 select sum(comm) as "커머션의 총합",
-         avg(comm) as "커머션의 평균",
+         avg(comm) as "커머션의 평균"
+from emp;
+
+
+-- distinct를 이용해서 중복된 개수 제거
+select count(distinct job) -- distinct 전엔 14개(중복된 직급들 포함)
+from emp
+;
+
+
+--마당서적 book 테이블에 책을 납품하는 출판사
+select count (distinct publisher) -- 중복된 출판사는 제거한 출판사의 개수 출력
+--select distinct publisher -- 중복된 출판사는 제거한 출판사 리스트 출력
+from book
+order by publisher
+;
+
+
+-- 특정 컬럼을 이용해서 그룹핑하기 -> group by
+-- 특정 조건을 기준으로 그룹핑하여 해당 특정 raw 데이터들을 다룬다. 
+-- select
+-- from 테이블
+-- where
+-- group by 그룹핑할 기준의 컬럼
+-- 위치 중요!
+
+
+
+
+
+
+
