a1626378d78085961c7a45311f4bd63d24be84ba
jiwon <xxxlikeag1_@naver.com>
2021-06-13 23:31:35 +0900

    Update 20210613_sql_exam04.sql

--- SQL/exam/20210613_sql_exam04.sql
+++ SQL/exam/20210613_sql_exam04.sql
@@ -12,34 +12,26 @@ where d.deptno = e.deptno and e.ename = 'SCOTT'
 --그 사원이 소속된 부서이름과 지역 명을 출력하시오.
 select e.ename, d.dname, d.loc
 from dept d inner join emp e on d.deptno = e.deptno
-​;
+;
+
+
+--36. 조인과 WildCARD를 사용하여 이름에 ‘A’가 포함된 모든 사원의 이름과 부서명을 출력하시오.
+select e.ename, d.dname
+from dept d, emp e
+where d.deptno = e.deptno and ename like '%A%'
+;
+​
+
+--37. JOIN을 이용하여 NEW YORK에 근무하는 모든 사원의 이름, 업무, 부서번호 및 부서명을 출력하시오.
+
 
 ​
 
-​
-
-36. 조인과 WildCARD를 사용하여 이름에 ‘A’가 포함된 모든 사원의 이름과 부서명을 출력하시오.
-
-​
-
-​
-
-​
-
-37. JOIN을 이용하여 NEW YORK에 근무하는 모든 사원의 이름, 업무, 부서번호 및 부서명을 출력하시오.
-
-​
-
-​
-
-​
-
-38. SELF JOIN을 사용하여 사원의 이름 및 사원번호, 관리자 이름을 출력하시오.
-
-​
-
-​
-
+--38. SELF JOIN을 사용하여 사원의 이름 및 사원번호, 관리자 이름을 출력하시오.
+select e.ename, e.empno, m.ename as "매니저 이름"
+from emp e, emp m
+where e.mgr = m.empno
+;
 ​
 
 39. OUTER JOIN, SELF JOIN을 사용하여 관리자가 없는 사원을 포함하여 사원번호를 기준으로 내림차순 정렬하여 출력하시오.
