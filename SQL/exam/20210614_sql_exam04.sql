b2128a69bc00cb8470a97239e9450a7a3c087aa6
jiwon <xxxlikeag1_@naver.com>
2021-06-14 13:07:21 +0900

    exam

--- SQL/exam/20210614_sql_exam04.sql
+++ SQL/exam/20210614_sql_exam04.sql
@@ -11,7 +11,7 @@ where d.deptno = e.deptno
 
 -- ANSI JOIN
 select e.deptno as "부서 번호", e.ename as "사원 이름", d.dname as "부서명"
-from emp e inner join dept d
+from emp e inner join dept d -- inner 생략 가능
 on e.deptno = d.deptno
 where e.ename = 'SCOTT'
 ;
@@ -20,18 +20,14 @@ where e.ename = 'SCOTT'
 --33. INNER JOIN과 ON 연산자를 사용하여 사원 이름과 함께
 --그 사원이 소속된 부서이름과 지역 명을 출력하시오.
 select e.ename, d.dname, d.loc
-from dept d inner join emp e on d.deptno = e.deptno
+from dept d inner join emp e
+on d.deptno = e.deptno -- join의 조건
+where e.ename = 'SCOTT' -- 행을 선택하는 조건
 ;
 
 
--- ANSI JOIN
-select e.ename, d.dname, d.loc
-from emp e inner join dept d
-on e.deptno = d.deptno
-;
-
-
---36. 조인과 WildCARD를 사용하여 이름에 ‘A’가 포함된 모든 사원의 이름과 부서명을 출력하시오.
+--36. 조인과 WildCARD를 사용하여
+--이름에 ‘A’가 포함된 모든 사원의 이름과 부서명을 출력하시오.
 select e.ename, d.dname
 from emp e, dept d
 where e.deptno = d.deptno and ename like '%A%'
@@ -40,7 +36,7 @@ where e.deptno = d.deptno and ename like '%A%'
 
 -- ANSI JOIN
 select e.ename, d.dname
-from emp e inner join dept d
+from emp e join dept d
 on e.deptno = d.deptno
 where e.ename like '%A%'
 ;
@@ -50,7 +46,8 @@ where e.ename like '%A%'
 --모든 사원의 이름, 업무, 부서번호 및 부서명을 출력하시오.
 select e.ename, e.job, e.deptno, d.dname
 from emp e, dept d
-where e.deptno= d.deptno and d.loc ='NEW YORK'
+where e.deptno= d.deptno
+and d.loc ='NEW YORK'
 ;
 
 
@@ -80,6 +77,7 @@ on e.mgr = m.empno
 select e.ename, e.empno, m.ename as "매니저 이름"
 from emp e, emp m
 where e.mgr = m.empno(+)
+order by e.empno desc
 ;
 ​
 
@@ -93,32 +91,38 @@ order by empno desc
 
 --40. SELF JOIN을 사용하여 지정한 사원의 이름, 부서번호,
 --지정한 사원과 동일한 부서에서 근무하는 사원을 출력하시오. ( SCOTT )
-select e.deptno, s.ename
+select ename from emp where deptno = 20;
+
+
+select e.deptno, e.deptno, s.ename
 from emp e, emp s
 where e.deptno = s.deptno and e.ename = 'SCOTT' 
 ;
 ​
 
 -- ANSI JOIN
-select e.deptno, s.ename
+select e.deptno, e.deptno, s.ename
 from emp e inner join emp s
 on e.deptno = s.deptno
 where e.ename = 'SCOTT' and not s.ename = 'SCOTT'
 ;
 
 
+select * from emp;
+
 --41. SELF JOIN을 사용하여 WARD 사원보다 늦게 입사한 사원의 이름과 입사일을 출력하시오.
 select e.ename, e.hiredate
 from emp e, emp w
-where w.ename = 'WARD' and e.hiredate > w.hiredate
+where w.ename = 'WARD'
+and e.hiredate > w.hiredate
 order by e.hiredate
 ;
 ​
 
 -- ANSI JOIN
-select e.ename, w.hiredate
+select e.ename, e.hiredate
 from emp e inner join emp w
-on e.hiredate < w.hiredate
+on e.hiredate > w.hiredate
 where w.ename = 'WARD'
 order by e.hiredate
 ;
