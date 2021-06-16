069b3da0b8c7b59225568bcaa144dc8afa4453d2
jiwon <xxxlikeag1_@naver.com>
2021-06-16 14:37:49 +0900

    SQL DML

--- SQL/20210616_sql_dml.sql
+++ SQL/20210616_sql_dml.sql
@@ -119,3 +119,75 @@ set hiredate = sysdate
 where substr(hiredate, 1,2) = '81'
 ;
 
+
+-- SCOTT 사원의 부서번호는 20번으로,
+-- 직급은 MANAGER로 한꺼번에 수정합시다.
+update emp01
+set deptno = 30, job = 'MANAGER'
+where ename = 'SCOTT'
+;
+
+
+-- SCOTT 사원의 입사일자는 오늘로,
+-- 급여를 50으로 커미션을 4000으로 수정합시다.
+update emp01
+set hiredate = sysdate, sal = 50, comm = 4000
+where ename = 'SCOTT'
+;
+
+
+-- 20번 부서의 지역명을
+-- 40번 부서의 지역명으로 변경하기 위해서 서브 쿼리문을 사용해 봅시다.
+insert into dept01
+select * from dept
+;
+
+drop table dept01;
+create table dept01
+as
+select * from dept
+;
+select * from dept01;
+
+-- 40번 부서의 지역명 : 
+select loc from dept01 where deptno = 40;
+
+update dept01
+set loc = 'BOSTON'
+where deptno = 20
+;
+-- 만약 지역이 바뀐다면 일일이 수정이 필요한 상황 
+-- 번거로움을 해결하기 위해 sub query 사용
+update dept01
+set loc = (select loc from dept01 where deptno = 40)
+where deptno = 20
+;
+
+
+-- 서브 쿼리를 이용해서
+-- 부서 번호가 20인 부서의 부서명과 지역명을
+-- 부서 번호가 10번인 부서와 동일하게 변경하도록 해봅시다.
+select * from dept01 where deptno = 10;
+select * from dept01 where deptno = 20;
+
+update dept01
+set (dname, loc) = (select dname, loc from dept01 where deptno = 10)
+where deptno = 20
+;
+
+select * from emp01;
+
+-- 데이터의 삭제 : 행 단위 삭제
+-- delete from 테이블 이름 where 조건
+
+-- dept01 테이블의 모든 데이터를 삭제
+delete from dept01;
+
+-- 이름이 SCOTT인 사원을 삭제
+delete from emp01 where ename = 'SCOTT';
+
+-- 사원 테이블에서 부서명이 SALES인 사원을 모두 삭제해봅시다. -- deptno : 30
+select * from dept where dname = 'SALES';
+-- delete from emp01 where deptno = 30;
+delete from emp01
+where deptno = (select deptno from dept where dname = 'SALES');
