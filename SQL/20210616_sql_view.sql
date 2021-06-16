c2ae5a608a7ce4fba02d47ba908dc4bc3f184e90
jiwon <xxxlikeag1_@naver.com>
2021-06-16 18:24:43 +0900

    SQL VIEW

--- SQL/20210616_sql_view.sql
+++ SQL/20210616_sql_view.sql
@@ -55,3 +55,15 @@ select rownum, ename, hiredate
 from view_HIRE
 where rownum < 6
 ;
+
+
+
+
+-- sequence : 번호 재생기
+create sequence pi_idx_pk;
+
+select pi_idx_pk.nextval
+from dual;
+
+select pi_idx_pk.currval
+from dual;
