e07419bdc6640ae6ac31a62a9fa1eff7df6a2bb9
jiwon <xxxlikeag1_@naver.com>
2021-06-18 18:30:14 +0900

    JDBC

--- JDBCTest/src/dept/DeptMain.java
+++ JDBCTest/src/dept/DeptMain.java
@@ -10,23 +10,64 @@ public class DeptMain {
 
 	public static void main(String[] args) {
 		
-		Connection conn = null;
+		Scanner sc = new Scanner(System.in);
+		DeptManager manager = new DeptManager(DeptDao.getInstance());
+//		Connection conn = null;
 
 		// 1. 드라이버 로드
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
-			System.out.println("드라이버 로드 성공!");
+			
+			
+			while (true) {
+				System.out.println("부서 관리 프로그램");
+				System.out.println("==========================");
+				System.out.println("1. 부서 리스트");
+				System.out.println("2. 부서 정보 입력");
+				System.out.println("3. 부서 정보 수정");
+				System.out.println("4. 부서 정보 삭제");
+				System.out.println("5. 프로그램 종료");
+				System.out.println("==========================");
+				System.out.println("원하시는 기능의 번호를 입력해주세요.");
+				int num = Integer.parseInt(sc.nextLine());
+				
+				switch(num) {
+				case 1 :
+					manager.deptList();
+					break ;
+				case 2 :
+					manager.inputData();
+					break ;
+				case 3 :
+					manager.editDept();
+					break ;
+				case 4 :
+					manager.delDept();
+					break ;
+				case 5 :
+					System.out.println("프로그램을 종료합니다.");
+					return ;
+				
+			}
+			
+				}
+		
 
-			// 2. 연결
-			String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
-			String user = "hr";
-			String pw = "tiger";
+			
+			
+		
+//			System.out.println("드라이버 로드 성공!");
+//
+//			// 2. 연결
+//			String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+//			String user = "hr";
+//			String pw = "tiger";
+//
+//			conn = DriverManager.getConnection(jdbcUrl, user, pw);
+//			System.out.println("데이터베이스 연결 성공!!!");
 
-			conn = DriverManager.getConnection(jdbcUrl, user, pw);
-			System.out.println("데이터베이스 연결 성공!!!");
-
-			DeptDao dao = DeptDao.getInstance();
+			// DeptDao dao = DeptDao.getInstance();
 
 			
 			/*List<Dept> list = dao.getDeptList(conn);
@@ -36,7 +77,7 @@ public class DeptMain {
 			}
 			*/
 			
-			Scanner sc = new Scanner(System.in);
+			// Scanner sc = new Scanner(System.in);
 			
 			/*System.out.println("부서입력을 시작합니다.");
 			System.out.println("부서번호 부서이름 부서위치 순으로 입력해주세요.");
@@ -70,27 +111,28 @@ public class DeptMain {
 				System.out.println("수정 실패!");
 			}*/
 			
-			
-			System.out.println("부서 정보를 삭제합니다");
-			System.out.println("삭제할 부서번호를 입력해주세요.");
-			String deptno = sc.nextLine();
-			int result = dao.deleteDept(conn, Integer.parseInt(deptno));
-			
-			
-			if (result > 0) {
-				System.out.println("삭제 완료");
-			} else {
-				System.out.println("삭제 실패 : 해당 번호의 부서가 존재하지 않습니다.");
-			}
-			
-			
+//			
+//			System.out.println("부서 정보를 삭제합니다");
+//			System.out.println("삭제할 부서번호를 입력해주세요.");
+//			String deptno = sc.nextLine();
+//			int result = dao.deleteDept(conn, Integer.parseInt(deptno));
+//			
+//			
+//			if (result > 0) {
+//				System.out.println("삭제 완료");
+//			} else {
+//				System.out.println("삭제 실패 : 해당 번호의 부서가 존재하지 않습니다.");
+//			}
+//			
+//			
 			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
-		
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
 
 	}
 
