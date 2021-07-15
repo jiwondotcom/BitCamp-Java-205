48de56e0ca254026abd964012f3f85631ceca87f
jiwon <xxxlikeag1_@naver.com>
2021-07-15 17:35:18 +0900

    commit

--- jsp_project/First-Web/src/main/java/firstWeb/HelloServlet.java
+++ jsp_project/First-Web/src/main/java/firstWeb/HelloServlet.java
@@ -7,7 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet({ "/hello", "/hello2" })
 public class HelloServlet extends HttpServlet {
+     
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
--- jsp_project/First-Web/src/main/java/firstWeb/NowServlet.java
+++ jsp_project/First-Web/src/main/java/firstWeb/NowServlet.java
@@ -10,32 +10,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HelloServlet")
+@WebServlet("/now2")
 public class NowServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("GET로 요청");
 		
-		resp.setContentType("text/html; chartset=utf-8");
+		resp.setContentType("text/html; charset=utf-8"); 
 		
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html>");
-		writer.println("	<head><title>NowServlet</title></head>");
+		writer.println("<head><title>NowServlet</title></head>");
 		writer.println("<body>");
-		writer.println("<h1>" + new Date() + "</h1>");
+		writer.println("<h1>"+new Date()+"</h1>");
 		writer.println("</body>");
 		writer.println("</html>");
 		
 		writer.close();
-	
+		
+		
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("POST로 요청");
 	}
+	
+	
 
-	
-	
 }
