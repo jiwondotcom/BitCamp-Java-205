4be05950b56b8915f682db264c1b2a787df4c656
jiwon <xxxlikeag1_@naver.com>
2021-07-21 16:18:29 +0900

    commit

--- SQL/20210721_mysql_NCS_memberManager.sql
+++ SQL/20210721_mysql_NCS_memberManager.sql
@@ -2,11 +2,11 @@ CREATE TABLE member.memberInfo (
   `USERID` varchar(14) COLLATE utf8_bin NOT NULL,
   `USERPW` varchar(14) COLLATE utf8_bin NOT NULL,
   `USERNAME` varchar(14) COLLATE utf8_bin NOT NULL,
-  `REGDATE` varchar(14) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY(`USERID`)
+  `REGDATE` varchar(50) COLLATE utf8_bin NOT NULL
   );
   
   drop table member.memberInfo;
+
   
   insert into member.memberInfo values ('jiwon', 2568, '황지원', sysdate());
   
