598169d857ac98ff686fedca8ee1fae81025e57b
jiwon <xxxlikeag1_@naver.com>
2021-07-23 11:51:02 +0900

    commit

--- jsp_project/First-Web/src/main/java/product/Product.java
+++ jsp_project/First-Web/src/main/java/product/Product.java
@@ -1,10 +1,53 @@
 package product;
 
+import java.util.Arrays;
+
 // beans 형식의 클래스로 정의 : 상품정보를 저장한다.
 public class Product {
 
 	// 상품 목록 : 배열 활용
 	private String[] productList = {"item1", "item2", "item3", "item4", "item5"};
+
+	// 변수 : el테스트용
+	private int price = 100;
+	private int amount = 1000;
+	
+	
+	public String[] getProductList() {
+		return productList;
+	}
+	public void setProductList(String[] productList) {
+		this.productList = productList;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
+	// ${product.display}
+	public String getDisplay() {
+		return "price : " + this.price + ", amount : " + this.amount;
+	}
+	
+	
+	@Override
+	// util.Arrays : 배열 모양으로 출력해준다.
+	public String toString() {
+		return "Product [productList=" + Arrays.toString(productList) + ", price=" + price + ", amount=" + amount + "]";
+	}
+	
+	
+	
+	
 	
 	
 	
---
+++ jsp_project/First-Web/src/main/webapp/el/productList.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<style>
+</style>
+<script>
+</script>
+<body>
+
+<!-- scope : 속성을 저장할 수 있다. -->
+<jsp:useBean id = "product" class = "product.Product" scope = "session"/>
+1. session 객체의 product
+
+</body>
+</html>
