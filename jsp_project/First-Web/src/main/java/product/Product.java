3a5c4d269f3e97eb545efd15b0f2f85b0e61acfe
jiwon <xxxlikeag1_@naver.com>
2021-07-23 11:44:08 +0900

    commit

--- jsp_project/First-Web/src/main/java/product/Product.java
+++ jsp_project/First-Web/src/main/java/product/Product.java
@@ -3,4 +3,9 @@ package product;
 // beans 형식의 클래스로 정의 : 상품정보를 저장한다.
 public class Product {
 
+	// 상품 목록 : 배열 활용
+	private String[] productList = {"item1", "item2", "item3", "item4", "item5"};
+	
+	
+	
 }
