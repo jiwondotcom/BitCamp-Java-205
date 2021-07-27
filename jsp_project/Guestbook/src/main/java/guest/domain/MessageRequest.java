47793512b20b2ecfa7a8f808fd2d03e4c8af2694
jiwon <xxxlikeag1_@naver.com>
2021-07-27 11:39:05 +0900

    commit

---
+++ jsp_project/Guestbook/src/main/java/guest/domain/MessageRequest.java
@@ -0,0 +1,30 @@
+package guest.domain;
+
+public class MessageRequest {
+	
+	private String guestName;
+	private String password;
+	private String message;
+
+	
+	public String getGuestName() {
+		return guestName;
+	}
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
