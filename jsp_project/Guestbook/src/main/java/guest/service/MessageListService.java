3954e1fa776ebc558dc9e0eb4e0abc587a49ae30
jiwon <xxxlikeag1_@naver.com>
2021-07-27 14:44:13 +0900

    commit

--- jsp_project/Guestbook/src/main/java/guest/service/MessageListService.java
+++ jsp_project/Guestbook/src/main/java/guest/service/MessageListService.java
@@ -1,5 +1,29 @@
 package guest.service;
 
+import guest.domain.MessageListView;
+
 public class MessageListService {
 
+	// 싱글톤 처리
+	private MessageListService() {}
+	
+	private static MessageListService service = new MessageListService();
+	
+	public static MessageListService getInstance() {
+		return service;
+	}
+	
+	
+	
+	public MessageListView getMessageList(int pageNumber) {
+		
+		MessageListView listView = null;
+		
+		
+		
+		
+		return listView;
+	}
+	
+	
 }
