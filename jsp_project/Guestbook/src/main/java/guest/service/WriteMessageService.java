c44cedf2068108c8743b7a6f9f15e591b5be1265
jiwon <xxxlikeag1_@naver.com>
2021-07-27 12:01:27 +0900

    commit

---
+++ jsp_project/Guestbook/src/main/java/guest/service/WriteMessageService.java
@@ -0,0 +1,16 @@
+package guest.service;
+
+import guest.domain.MessageRequest;
+
+public class WriteMessageService {
+
+	public int writeMessage(MessageRequest msRequest) {
+		
+		int resultCnt = 0;
+		
+		return resultCnt;
+	}
+
+
+
+}
