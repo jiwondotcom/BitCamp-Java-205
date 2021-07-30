c9982925b19852f7b5f4ba6a4cd9056901dcfb9b
jiwon <xxxlikeag1_@naver.com>
2021-07-30 10:14:23 +0900

    commit

--- jsp_project/Junit4Test/src/main/java/cal/Calculator.java
+++ jsp_project/Junit4Test/src/main/java/cal/Calculator.java
@@ -18,4 +18,18 @@ public class Calculator {
 		return num1 / num2;
 	}
 	
+	
+	public static void main (String[] args) {
+		Calculator cal = new Calculator();
+		
+		// 덧셈 테스트
+		System.out.println(cal.add(10,2)); // 12
+		// 뺄셈 테스트
+		System.out.println(cal.substract(10,2)); // 8
+		// 곱셈 테스트
+		System.out.println(cal.multiply(10, 2)); // 20
+		// 나눗셈 테스트
+		System.out.println(cal.divide(10,2)); // 5
+		
+	}
 }
