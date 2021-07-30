18135d8068cbbc3d68218fbee5027c3a9dd58fdb
jiwon <xxxlikeag1_@naver.com>
2021-07-30 10:59:42 +0900

    commit

--- jsp_project/Junit4Test/test/cal/CalculatorTest.java
+++ jsp_project/Junit4Test/test/cal/CalculatorTest.java
@@ -9,46 +9,63 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
-
+	
+	// Calculator cal = new Calculator();
+	Calculator cal;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("@BeforeClass");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		System.out.println("@AfterClass");
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("@Before");
+		cal = new Calculator();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		System.out.println("@After");
 	}
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		System.out.println("testAdd()");
+		
+		// Calculator cal = new Calculator();
+		
+		assertEquals("add() 메소드 테스트", 12, cal.add(10, 2));
+		
+		// System.out.println(cal.add(10, 2));
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSubstract() {
-		fail("Not yet implemented");
+		System.out.println("testSubstract()");
+		assertEquals("substract() 메소드 테스트", 8, cal.substract(10, 2));
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		System.out.println("testMultiply()");
+		assertEquals("multiply() 메소드 테스트", 20, cal.multiply(10, 2));
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		System.out.println("testDivide()");
+		assertEquals("divide() 메소드 테스트", 5, cal.divide(10, 2));
+		// fail("Not yet implemented");
 	}
 
-	@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}
 
 }
