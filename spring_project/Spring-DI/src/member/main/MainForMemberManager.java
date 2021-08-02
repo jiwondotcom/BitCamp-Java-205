3637b798e3f66df53d6ee2459bf60103200bfb96
jiwon <xxxlikeag1_@naver.com>
2021-08-02 16:02:50 +0900

    commit

---
+++ spring_project/Spring-DI/src/member/main/MainForMemberManager.java
@@ -0,0 +1,98 @@
+package member.main;
+
+import java.util.Scanner;
+
+import member.dao.MemberDao;
+import member.domain.RegRequest;
+import member.service.ChangePasswordService;
+import member.service.MemberRegService;
+
+public class MainForMemberManager {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			printMenu();
+			
+			String command = sc.nextLine();
+			// 프로그램의 종료 분기
+			if(command.equalsIgnoreCase("exit")) {
+				System.out.println("프로그램 종료");
+				return;
+			}
+			
+			if(command.startsWith("new")) {
+				String[] values = command.split(" ");
+				// values[0] => abc@gamil.com
+				// values[1] => abc
+				// values[2] => 1234 
+				// .....
+				processNewMember(values);
+				continue;
+			}
+			
+			
+			if(command.startsWith("change")) {
+				String[] values = command.split(" ");
+				processChangePW(values);
+			}
+		}
+		
+	}
+	
+	private static void processChangePW(String[] values) {
+		// MemberRegService를 이용해서 패스워드를 변경한다.
+		
+		ChangePasswordService service = new ChangePasswordService(new MemberDao());
+		
+		service.changePassword(values[1], oldPW, newPW);
+	}
+
+	
+	
+	private static void processNewMember(String[] values) {
+		// MemberRegService를 이용해서 정보를 저장한다.
+		
+		MemberRegService service = new MemberRegService(new MemberDao());
+		RegRequest request = new RegRequest();
+		request.setEmail(values[1]);
+		request.setName(values[2]);
+		request.setPassword(values[3]);
+		request.setConfirmPassword(values[4]);
+		
+		if(!request.isPasswordEqualToConfirmPassword()) {
+			// 같을때  true, 다를때 false
+			System.out.println("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+			return; 
+		} 
+		
+		try {
+			service.regMember(request);
+			System.out.println("정상적으로 등록되었습니다.");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+			
+	}
+
+	static void printMenu() {
+		System.out.println();
+		System.out.println("명령어 사용법");
+		System.out.println("=================================");
+		System.out.println(">>>>>>>>>> 회원 등록 <<<<<<<<<<");
+		System.out.println("new 이메일 이름 비밀번호 비밀번호 확인");
+		System.out.println("new abc@gmail.com abc 1234 1234");
+		System.out.println(">>>>>>>>>> 회원 수정 <<<<<<<<<<");
+		System.out.println("change 이메일 현재 비밀번호 새로운 비밀번호");
+		System.out.println("change abc@gmail.com 1234 0000");
+		System.out.println("=================================");
+	
+	}
+	
+	
+	
+}
--- spring_project/Spring-DI/src/member/service/ChangePasswordService.java
+++ spring_project/Spring-DI/src/member/service/ChangePasswordService.java
@@ -1,5 +1,28 @@
 package member.service;
 
+import member.dao.Dao;
+import member.domain.Member;
+
 public class ChangePasswordService {
 
+	Dao dao;
+	
+	public ChangePasswordService(Dao dao) {
+		this.dao = dao;
+	}
+	
+	public void changePassword(String email, String oldPW, String newPW) throws Exception {
+		
+		Member member = dao.selectByEmail(email);
+		
+		if(member == null) {
+			throw new Exception("존재하지 않는 회원입니다.");
+		}
+		
+		member.changePassword(oldPW, newPW);
+		
+		dao.update(member);
+		
+	}
+	
 }
