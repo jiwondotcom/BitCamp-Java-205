b5062f0b9727ed5f5a2855e2d01e0f5f8a4b1b41
jiwon <xxxlikeag1_@naver.com>
2021-08-04 15:12:12 +0900

    commit

--- spring_project/firstSpring/src/main/java/com/bitcamp/firstSpring/OrderController.java
+++ spring_project/firstSpring/src/main/java/com/bitcamp/firstSpring/OrderController.java
@@ -2,9 +2,17 @@ package com.bitcamp.firstSpring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/order/orderForm")
 public class OrderController {
 
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getOrderForm() {
+		return "order/orderForm";		// /WEB-INF/views/order/orderForm.jsp
+	}
+	
+	
 }
