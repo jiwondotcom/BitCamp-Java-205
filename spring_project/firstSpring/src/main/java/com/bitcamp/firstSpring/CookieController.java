45f4dd5417dd171adac2023235a6bf326e6a91e5
jiwon <xxxlikeag1_@naver.com>
2021-08-05 10:20:40 +0900

    commit

--- spring_project/firstSpring/src/main/java/com/bitcamp/firstSpring/CookieController.java
+++ spring_project/firstSpring/src/main/java/com/bitcamp/firstSpring/CookieController.java
@@ -4,19 +4,21 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CookieController {
 
-	
 		// 쿠키를 생성해주는 요청 처리
 		@RequestMapping(value = "/cookie/make")
 		public String makeCookie(HttpServletResponse response) {
 			
 			Cookie c = new Cookie("userName", "jiwon");
 			response.addCookie(c);
+			
+			// Service.process(response)
 			
 			return "cookie/make";
 		}
@@ -26,12 +28,15 @@ public class CookieController {
 		@RequestMapping("cookie/view")
 		public String viewCookie(
 				
-				@CookieValue("userName") String userName
+//				@CookieValue(value = "userNames", required = true) String userName, // 반드시 가져와야만 할때 require문
+				@CookieValue(value = "userNames", defaultValue = "goo") String userName, // 없다면 기본값 처리해야 할 때
+				Model model
 				
 				) {
 			
+			model.addAttribute("userName", userName);
+			System.out.println(userName);
 			// ..Cookie 정보를 이용해서 데이터 처리
-			
 			
 			return "cookie/view";
 		}
