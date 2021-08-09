987a1d35946a8e2f343cb9715bb6b43a1376d999
jiwon <xxxlikeag1_@naver.com>
2021-08-09 10:16:06 +0900

    commit

--- spring_project/OpenProject/src/main/java/com/bitcamp/op/indexController.java
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/indexController.java
@@ -2,6 +2,7 @@ package com.bitcamp.op;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class indexController {
@@ -14,6 +15,14 @@ public class indexController {
 	public String index1() {
 		System.out.println("/ 진입");
 		return "redirect:/index";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String login() {
+		
+		// 사용자가 입력한 ID와 PW를 서비스에 전달 -> 로그인 처리
+		
+		return "member/login";
 	}
 	
 }
