1968c692c937b82b5c33d044f79b83634e45a7d5
jiwon <xxxlikeag1_@naver.com>
2021-08-17 11:28:06 +0900

    commit

---
+++ spring_project/firstSpring/src/main/java/com/bitcamp/firstSpring/RestTestController.java
@@ -0,0 +1,35 @@
+package com.bitcamp.firstSpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping
+public class RestTestController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getForm() {
+		return "rest/form";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String simple(
+			
+			@RequestBody String body
+			
+			) {
+		
+		// name = cool & age = 11
+		// JSON 데이터를 받을 때 이용
+		// GSON 라이브러리 : JSON -> Java Object
+		System.out.println("body :" + body);
+		
+		return body;
+		//return "@ResponseBody를 이용한 응답";
+	
+	}
+	
+
+}
