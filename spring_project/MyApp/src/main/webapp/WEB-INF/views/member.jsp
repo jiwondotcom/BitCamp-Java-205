d9cea57ce263a87d4c4812620f9c52f07ad10b79
jiwon <xxxlikeag1_@naver.com>
2021-08-17 15:34:36 +0900

    commit

--- spring_project/MyApp/src/main/webapp/WEB-INF/views/member.jsp
+++ spring_project/MyApp/src/main/webapp/WEB-INF/views/member.jsp
@@ -6,9 +6,59 @@
 <head>
 <meta charset="UTF-8">
 <title>회원 리스트</title>
+<style>
 
-<script src = "http://code.jquery.com/jquery-1.12.4.min.js"></script>
+	#memberlist {
+		overflow : hidden;
+	}
 
+	div.card {
+		float : left;
+		width : 28%;
+		padding : 10px;
+		border : 1px solid #AAA;
+		border-radius : 5px;
+		margin : 5px;
+	}
+
+
+</style>
+
+<script src = "https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script>
+	
+	$(document).ready(function() {
+		
+		$.ajax({
+			url : 'http://localhost:8080/op/members',
+			type : 'GET',
+			success : function(data) {
+				// console.log(data);
+				$.each(data, function(index, item){
+					console.log(index, item);
+					
+					var html = '<div class = "card">';
+					html += "index : " + item.index + "<br>";
+					html += "아이디 : " + item.userID + "<br>";
+					html += "이름 : " + item.userName + "<br>";
+					html += "사진 : " + item.userPhoto + "<br>";
+					html += "등록일 : " + item.regDate + "<br>";
+					html += "<div>"
+					
+					$('#memberlist').append(html);
+					
+				});
+			}
+			
+			
+		});
+		
+		
+		
+	});
+
+
+</script>
 </head>
 <body>
 
--- spring_project/OpenProject/src/main/java/com/bitcamp/op/member/controller/MemberRestController.java
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/controller/MemberRestController.java
@@ -14,17 +14,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bitcamp.op.member.domain.Member;
 import com.bitcamp.op.member.domain.MemberRegRequest;
 import com.bitcamp.op.member.service.MemberRestService;
 import com.bitcamp.op.member.service.RegService;
 
-@Controller
+//@Controller
+@RestController
 public class MemberRestController {
 	
 	@Autowired
 	private MemberRestService restService;
+	
+	@Autowired
+	private RegService regService;
+	
 	
 	@RequestMapping("/members/{id}")
 	public Member getMember(
@@ -34,7 +40,6 @@ public class MemberRestController {
 		System.out.println(member);
 		return member;
 	}
-	
 	@GetMapping("/members")
 	public List<Member> getMembers(){
 		return restService.getMembers();
@@ -52,8 +57,9 @@ public class MemberRestController {
 			HttpServletRequest request
 			) {
 		System.out.println(regRequest);
-		return Integer.toString(RegService.regMember(regRequest, request));
+		return Integer.toString(regService.regMember(regRequest, request));
 	}
+	
 	
 	@PostMapping("/members/reg2")
 	public String regMember2(
@@ -61,7 +67,8 @@ public class MemberRestController {
 			HttpServletRequest request
 			) {
 		System.out.println(regRequest);
-		return Integer.toString(RegService.regMember(regRequest, request));
+		return Integer.toString(regService.regMember(regRequest, request));
 	}
 	
 }
+
