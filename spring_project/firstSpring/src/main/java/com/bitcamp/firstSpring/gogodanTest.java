b20e0100df196fa5fd43ea0d98d64f710a356835
jiwon <xxxlikeag1_@naver.com>
2021-10-23 23:48:39 +0900

    commit

---
+++ spring_project/firstSpring/src/main/java/com/bitcamp/firstSpring/gogodanTest.java
@@ -0,0 +1,15 @@
+package com.bitcamp.firstSpring;
+
+public class gogodanTest {
+
+	public static void main(String[] args) {
+		for(int i = 1; i < 10; i++) {
+			System.out.println();
+			for(int j = 1; j < 10; j++) {
+				System.out.println(i + "단: " + i + "x" + j + " = " + i*j);
+			}
+		}
+		
+	}
+
+}
